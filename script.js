fc8f34a0c07e3d1d13a74080c08f930d9e26b465
Matheus Gouvêa <145941871+TeusGouvea@users.noreply.github.com>
2024-04-06 12:22:46 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+function toggleMode(){
+    const html = document.documentElement
+    html.classList.toggle("light")
+
+    const img = document.querySelector("#profile img")
+    
+
+    if(html.classList.contains('light')){
+        img.setAttribute('src', './assets/avatar-light.png')
+        img.setAttribute('alt', 'Foto de Matheus Gouvêa AAR ')
+
+    }else {
+
+        img.setAttribute('src', './assets/avatar.png')
+        img.setAttribute('alt', 'Foto de Matheus Gouvêa black and white')
+    }
+
+//rollReveal().reveal('ul li', { distance: '400px', origin: 'left', duration: 500, easing: 'ease-in-out'});
+//ScrollReveal().reveal('ul li.right', { distance: '400px', origin: 'right', duration: 500, easing: 'ease-in-out'});
+
+//ScrollReveal().reveal('#profile', { distance: '400px', origin: 'top', duration: 500, easing: 'ease-in-out'});
+
+//ScrollReveal().reveal('.links-sociais, #social-links, footer', { distance: '400px', origin: 'top', duration: 500, easing: 'ease-in-out'});
+
+//ScrollReveal().reveal('#switch', { distance: '0px', opacity: 0, duration: 1000, easing: 'ease-in-out'});
+
+}
